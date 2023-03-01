f369b4ab8e293fde9cd81e10554e3460e2f95528
GediminasA <galzbutas@gmail.com>
2023-03-01 17:33:15 +0200

    clusterstrees

--- notebooks/evaluate_cluster.r.R
+++ notebooks/evaluate_cluster.r.R
@@ -21,8 +21,9 @@ m1 = mnths[[1]]
 m2 = mnths[[length(mnths)]]
 
 df2 <- cldf2 %>%
-    filter(month == m1 | month ==m2)
+    filter(month == m1 | month == m2)
 df3 <- df2  %>%
+    filter(ClusterNumber != -1) %>%
     group_by(ClusterNumber, month) %>%
     summarise(N=n()) %>%
     tidyr::pivot_wider(names_from = month, values_from = N) %>%
@@ -32,10 +33,10 @@ df3 <- df2  %>%
     as.data.frame()
 df3$ratio <- df3[,2] / df3[,3]
 good <- df3 %>%
-    filter(total_nmb >= 100) %>%
+    filter(total_nmb >= 300) %>%
     filter(ratio < 1, ratio > 0.1)
 
 
 out=data.frame(N=nrow(good), Nall=nrow(df3), snakemake@wildcards$p,
-              meanv = mean(good$total_nmb), minv = min(good$total_nmb), maxv = max(good$total_nmb), sdv = sd(good$total_nmb))
+              meanv = mean(good$total_nmb), medianv = median(good$total_nmb), minv = min(good$total_nmb), maxv = max(good$total_nmb), sdv = sd(good$total_nmb))
 fwrite(x = out, col.names = F, file = snakemake@output[[1]])
