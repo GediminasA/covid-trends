e35a3773746d9b9deede8c473af21a64badd1e5e
GediminasA <galzbutas@gmail.com>
2023-08-31 18:34:55 +0000

    remove deopendancies on several lins

--- example_command.sh
+++ example_command.sh
@@ -1,3 +1,2 @@
-snakemake --use-conda --use-singularity -c 48  --configfile configs/example.yaml -k summarise_analyse_contact_enrichment 
-snakemake -c 32 --use-conda --configfile configs/config.yaml --edit-notebook audines11/rez/lineages/common/common_ref_derep_data.csv
 snakemake -c 32 --use-conda --configfile configs/config.yaml -f audines11/rez/pangolin_lineage_report.csv
+snakemake --use-conda --use-singularity -c 48  --configfile configs/example.yaml -k assemble_raport
